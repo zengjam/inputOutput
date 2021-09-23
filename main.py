f1dcf710f12970c97ce917183a46c548d245ca51
zengjam <91277235+zengjam@users.noreply.github.com>
2021-09-23 09:02:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#x gets 6
+x = 6
+# y gets 7
+y = 7
+#variable gets 9
+variable = 9
+#print what i stored in variable
+print(variable)
+#prints exactly what is in the quotes
+print("variable")
+#z gets the user input
+z = input("Enter a number between 1 and 10:")
+#print exactly "this is your number" and then print variable 'z'
+print("this is your number",z)
